67fc8401ebcd30a18a5166d963c9450c8ed26761
SimDuch <102703713+SimDuch@users.noreply.github.com>
2024-03-11 15:15:41 -0400

    Fixed device simulator and demo scene
    debug device simulator et demo alpha dans basic scene

--- Assets/Scripts/LaserReceiverController.cs
+++ Assets/Scripts/LaserReceiverController.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class LaserReceiverController : MonoBehaviour
 {
     public bool activated = false;
+    public Color activatedColor = Color.green; // couleur qu'on change quand activé
+    private Renderer renderer; // Le rendu du receiver pour changer la couleur
+
+    void Start()
+    {
+        // le render du receiver
+        renderer = GetComponent<Renderer>();
+    }
 
     public void TriggerLaserHit()
     {
         if(!activated)
         {
             activated = true;
+            // change la couleur en vert
+            renderer.material.color = activatedColor;
         }
     }
 
@@ -19,6 +29,8 @@ public class LaserReceiverController : MonoBehaviour
         if (activated)
         {
             activated = false;
+            // On remet la couleur de départ
+            renderer.material.color = Color.grey; // à modifié selon la couleur souhaité
         }
     }
 }
